8b201ac0ab6634ef29b9b9a31609412679189952
alexiscenriquez <alexiscenriquez@hotmail.com>
2021-09-11 21:39:40 -0500

    js work

--- index.js
+++ index.js
@@ -1,39 +1,29 @@
-let button = document.querySelector("button");
+let button = document.querySelector("#share");
 let bar = document.querySelector("#bar");
-let person = document.querySelector("#person")
-let x = window.matchMedia("(max-width:700px)")
-let profile = document.querySelector("#profile")
+let person = document.querySelector("#person");
+let x = window.matchMedia("(max-width:700px)");
+let profile = document.querySelector("#profile");
+let profileWrapper = document.querySelector("#profile-wrapper")
 function toggleClass(el) {
   if (el.className == "share") {
+    document.getElementById("bar").style.visibility = "visible";
     el.className = "share2";
-    bar.style.visibility = "visible";
-
-  }
-  else {
+  
+  } else {
     el.className = "share";
     bar.style.visibility = "hidden";
+   
   }
+if(x.matches  && el.className=="share2"){
 
-  if (x.matches) {
-    if (el.className == "share") {
+/*profile.innerHTML= `
+<div id="inner-bar"><img class="icons" src="images/icon-facebook.svg" alt="">
+  <img src="images/icon-twitter.svg" alt="" class="icons">
 
-      bar.style.visibility = "hidden"
-    }
-    else {
-     
-      bar.style.visibility = "visible"
-  button.style.visibility = "hidden"
-    profile.innerHTML = `<div id="bar">
-    <div id="left">Share
-      <img class="icons" src="images/icon-facebook.svg" alt="">
-      <img src="images/icon-twitter.svg" alt="" class="icons">
-      <img src="images/icon-pinterest.svg" alt="" class="icons">
-      </div>
-      <button class="share" onclick="toggleClass(this)"></button>
-    </div>`;
-  
-  }}
+<img src="images/icon-pinterest.svg" alt="" class="icons"></div>  <button id="share" class="share" onclick="toggleClass(this)"></button>`
+}
+else{
 
-
-
+//  bar.remove(button)
+*/}
 }
